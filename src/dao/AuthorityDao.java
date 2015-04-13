73c9e316e162a1c3e6dcec26072c40dc3910dfc4
suanwumei <365719530@qq.com>
2015-04-14 00:30:06 +0800

    AuthorityDao的二次更新

--- src/dao/AuthorityDao.java
+++ src/dao/AuthorityDao.java
@@ -43,6 +43,7 @@ public class AuthorityDao extends HibernateDaoSupport
 	 * @param Authority
 	 * @return
 	 */
+	@Transactional
 	public String saveAuthority(Authority Authority) throws SQLException
 	{
 		try
